61fab32474e269ff1b177ef8dfd0780f64f7399d
Henrique <Carlos Henrique>
2021-04-01 21:15:31 -0300

    delete files

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-require('dotenv/config');
-
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: "https://sindsempi.herokuapp.com/"
 });
 
 
